3be7de2dff96aebfefb0b96159bd4cbf9330d4e9
AnikHaque <ekramul15-811@diu.edu.bd>
2025-04-30 01:13:04 +0600

    retrieved input values

--- calculator.js
+++ calculator.js
@@ -1,1 +1,6 @@
-function calculate() {}
+function calculate() {
+  // Retrieve input values
+  var num1 = parseFloat(document.getElementById("num1").value);
+  var num2 = parseFloat(document.getElementById("num2").value);
+  var operator = document.getElementById("operator").value;
+}
